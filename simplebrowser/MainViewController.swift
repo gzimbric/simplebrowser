8f91111d91794c3456075f3704f3f3865a162a9f
Gabe Zimbric <g.zimbric@gmail.com>
2017-11-24 21:13:54 -0600

    nightly #01104
    -updated logo

--- simplebrowser/MainViewController.swift
+++ simplebrowser/MainViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 import SafariServices
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var urlButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(MainViewController.dismissKeyboard)))
+        
         self.searchButton.layer.cornerRadius = 5
         self.searchButton.alpha = 0.70
         self.urlButton.layer.cornerRadius = 5
         self.urlButton.alpha = 0.70
+        
+        self.urlTextField.delegate = self;
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func dismissKeyboard() {
+        urlTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
     
     @IBAction func urlBrowser(_ sender: Any) {
